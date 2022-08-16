0f421539c5c78f74eb401b6a4aa2e082ec4f1f01
Sagi bar-shai <sagibarshai1@gmail.com>
2022-08-16 19:28:09 +0300

    start to build favorites page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Header from "./components/Header";
 import BackgroundAnimation from "./shared/backgroundAnimation/BackgroundAnimation";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Favorites from "./pages/Favorites";
 
 const App: React.FC = () => {
      return (
@@ -12,7 +12,7 @@ const App: React.FC = () => {
                <Routes>
                     <Route element={<Navigate to="/home" />} path="/" />
                     <Route element={<Home />} path="/home" />
-                    <Route element={<Home />} path="/favorite" />
+                    <Route element={<Favorites />} path="/favorites" />
                     <Route element={<Login />} path="/login" />
                </Routes>
                {/* <ReactQueryDevtools initialIsOpen={false} /> */}
