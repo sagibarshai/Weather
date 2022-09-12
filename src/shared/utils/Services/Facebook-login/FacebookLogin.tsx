5489c2127fd687f3fa019fb7f90829efc6639215
Sagi bar-shai <sagibarshai1@gmail.com>
2022-09-12 18:02:55 +0300

    smallfix

--- src/shared/utils/Services/Facebook-login/FacebookLogin.tsx
+++ src/shared/utils/Services/Facebook-login/FacebookLogin.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 import { StyledButton } from "../../../UIElements/Button/Button";
 import { StyledIcon } from "../../../Icons/Icon";
@@ -7,49 +8,69 @@ import { useMutation } from "react-query";
 import { Props } from "./types";
 import { login } from "../../../../redux/authSlice";
 import { useDispatch } from "react-redux";
-const LoginWithFacebook: React.FC<Props> = (props) => {
-     const dispatch = useDispatch();
-     const { mutate } = useMutation(loginFacebookService, {
-          onSuccess: (data: { token: string }) => {
-               dispatch(login(data.token.toString()));
-          },
-          onError: (err: any) => {
-               let errorMessage = "";
-               const errorsObj = err.request.response;
-               const parsesErrorsObj = JSON.parse(errorsObj);
-               for (let error in parsesErrorsObj) {
-                    if (Array.isArray(parsesErrorsObj[error])) {
-                         for (let element of parsesErrorsObj[error]) {
-                              errorMessage += element;
-                         }
-                    } else console.log(error);
-               }
-               props.setServerError(errorMessage);
-          },
-     });
 
+import { signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+import firebase from "firebase/auth";
+const LoginWithFacebook: React.FC<Props> = (props) => {
+     // const dispatch = useDispatch();
+     // const { mutate } = useMutation(loginFacebookService, {
+     //      onSuccess: (data: { token: string }) => {
+     //           dispatch(login(data.token.toString()));
+     //      },
+     //      onError: (err: any) => {
+     //           let errorMessage = "";
+     //           const errorsObj = err.request.response;
+     //           const parsesErrorsObj = JSON.parse(errorsObj);
+     //           for (let error in parsesErrorsObj) {
+     //                if (Array.isArray(parsesErrorsObj[error])) {
+     //                     for (let element of parsesErrorsObj[error]) {
+     //                          errorMessage += element;
+     //                     }
+     //                } else console.log(error);
+     //           }
+     //           props.setServerError(errorMessage);
+     //      },
+     // });
+     const provider = new FacebookAuthProvider();
+     const uiConfig = {
+          singInFlow: "popup",
+          singInOptions: [firebase.auth.FacebookAuthProvider.ProviderId],
+          callbackes: {
+               singInSuccess: () => false,
+          },
+     };
      return (
-          <FacebookLogin
-               appId={process.env.REACT_APP_FACEBOOK_LOGIN_APP_ID || ""}
-               callback={(res: any) =>
-                    mutate({ access_token: res.accessToken })
-               }
-               disableMobileRedirect={true}
-               render={(renderProps) => {
-                    return (
-                         <StyledButton
-                              variant="linkWithImg"
-                              type="button"
-                              onClick={renderProps.onClick}
-                         >
-                              <StyledIcon marginRight="8px">
-                                   <IconFacebook />
-                              </StyledIcon>
-                              Log in with facebook
-                         </StyledButton>
-                    );
-               }}
-          />
+          // <FacebookLogin
+          //      appId={
+          //           // process.env.REACT_APP_FACEBOOK_LOGIN_APP_ID ||
+          //           "3303782009865883"
+          //      }
+          //      callback={(res: any) => {
+          //           mutate({ access_token: res.accessToken });
+          //      }}
+          //      autoLoad={true}
+          //      disableMobileRedirect={true}
+          //      onClick={(e) => console.log(e)}
+          //      render={(renderProps) => {
+          //           return (
+          // <StyledButton
+          //      variant="linkWithImg"
+          //      type="button"
+          // >
+          //      <StyledIcon marginRight="8px">
+          //           <IconFacebook />
+          //      </StyledIcon>
+          //      Log in with facebook
+          // </StyledButton>
+          // );
+          // }}
+          // />
+          <StyledButton variant="linkWithImg" type="button">
+               <StyledIcon marginRight="8px">
+                    <IconFacebook />
+               </StyledIcon>
+               Log in with facebook
+          </StyledButton>
      );
 };
 export default LoginWithFacebook;
